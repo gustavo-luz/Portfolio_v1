bf46f4d0fdbabee6f9094b3aa113ec379e2e44cb
gustavo-luz <gustavo.luz.unb@gmail.com>
2021-03-24 23:30:49 -0300

    .

--- app.py
+++ app.py
@@ -4,43 +4,11 @@ import smtplib
 import os
 app = Flask(__name__)
 
-def mail(email, pssd, mssg):
-    
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(email, pssd)
-    server.sendmail(email, 'gustavo.luz.unb@gmail.com', mssg)
-
-    server.quit()
-
-
-
 
 @app.route('/')
 def home_page():
     return render_template("index.html")
 
-
-def cleanMsg(data):
-    name= data.get('name')
-    email= data.get('email')
-    msg=data.get('message')
-    message = f"""From: {name} <{email}>
-Subject: New Message On Our Official Website
-{msg}
-"""
-    return message
-
-@app.route('/submit_form',methods=['GET', 'POST'])
-def form():
-    if request.method == 'POST':
-        data = request.form.to_dict()
-        msg=cleanMsg(data)
-        password=os.environ.get('Password_Gmail')
-        mail('wantech010@gmail.com', password, msg)
-        return render_template("index.html",code="test()")
-    else:
-        return f"Something Not Right "
     
 
 @app.errorhandler(404)  
